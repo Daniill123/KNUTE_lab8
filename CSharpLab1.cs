2f158fa23a17f70864dbc9f994c15ffc1c780ea0
Daniill123 <107209551+Daniill123@users.noreply.github.com>
2022-06-09 19:32:44 +0200

    Create CSharpLab1.cs

---
+++ CSharpLab1.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Прізвище та ім'я: Плотніков Данііл");
+Console.WriteLine("Факультет: ФІТ (Факультет Інформаційних Технологій)");
+Console.WriteLine("Спеціальні");
+Console.WriteLine("Форма навчання: Денна");
+Console.WriteLine("Група: 1-2");
